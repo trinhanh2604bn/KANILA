943f905fea3d8790c1d34309d683997111c64cae
Ngoc Anh <anhngoctrinh2604@gmail.com>
2026-03-30 11:23:26 +0700

    update home

--- client/src/app/features/home/pages/components/divider/divider.ts
+++ client/src/app/features/home/pages/components/divider/divider.ts
@@ -1,9 +1,10 @@
 import { Component, Input } from '@angular/core';
+import { NgIf } from '@angular/common';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-divider',
-  imports: [RouterLink],
+  imports: [RouterLink, NgIf],
   templateUrl: './divider.html',
   styleUrl: './divider.css',
 })
